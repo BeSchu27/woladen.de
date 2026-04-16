178f39f5c469b14d0c313b645127cdf08220a9d7
raphaelvolz <rv@nogago.com>
2026-04-16 14:38:56 +0200

    fix(deploy): defer backend api imports

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,10 @@
-from .api import create_app
 from .config import AppConfig
 from .service import IngestionService
 
 __all__ = ["AppConfig", "IngestionService", "create_app"]
+
+
+def create_app(*args, **kwargs):
+    from .api import create_app as _create_app
+
+    return _create_app(*args, **kwargs)
